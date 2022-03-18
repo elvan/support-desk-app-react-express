463806c042c267fcd5eb107449e799c429db6223
Elvan H <elvanhd@gmail.com>
2022-03-18 22:26:30 +0700

    basic express server setup

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+const dotenv = require('dotenv');
+const express = require('express');
+
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
+});
