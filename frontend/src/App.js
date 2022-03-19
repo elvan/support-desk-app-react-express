32f745fa77b71d8ffa1955575e6205fc1453602c
Elvan H <elvanhd@gmail.com>
2022-03-19 11:20:44 +0700

    header and initial pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,24 @@
-export default function App() {
-  return <div>SupportDesk</div>;
-}
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+};
+
+export default App;
