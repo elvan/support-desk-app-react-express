79a478e1023e80868a4580a08c18b65f9c7bd21e
Elvan H <elvanhd@gmail.com>
2022-03-19 17:47:30 +0700

    route guard

---
+++ frontend/src/hooks/useAuthStatus.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  // @ts-ignore
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+
+    setCheckingStatus(false);
+    return () => {};
+  }, [user]);
+
+  return {
+    loggedIn,
+    checkingStatus,
+  };
+};
